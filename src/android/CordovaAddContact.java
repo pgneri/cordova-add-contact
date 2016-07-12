c614908b6279096bd923a1ef304863a68477a1f4
João Pedro Perondini <jpfperondini@gmail.com>
2016-07-12 15:54:38 -0300

    Remove android unecessary method

--- src/android/CordovaAddContact.java
+++ src/android/CordovaAddContact.java
@@ -28,12 +28,4 @@ public class CordovaAddContact extends CordovaPlugin {
         }
         return false;
     }
-
-    private void addContact(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
-    }
 }
